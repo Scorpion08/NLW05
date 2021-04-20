b1c797b8df13aab75b568e526576740fbabad0a5
Wesley Bonaccorsi <wesley_bonaccorsi@hotmail.com>
2021-04-20 16:08:35 -0300

    First commit

---
+++ 1618943642558-CreateSettings.ts
@@ -0,0 +1,44 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateSettings1618943642558 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name:"Settings",
+                columns:[
+                    {
+                        name:"id",
+                        type:"uuid",
+                        isPrimary:true
+                    },
+                    {
+                        name:"username",
+                        type:"varchar"
+                    },
+                    {
+                        name:"chat",
+                        type:"boolean",
+                        default:true
+                    },
+                    {
+                        name:"updated_at",
+                        type:"timestamp",
+                        default:"now()"
+                    },
+                    {
+                        name:"created_at",
+                        type:"timestamp",
+                        default:"now()"
+                    }
+
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("Settings");
+    }
+
+}
